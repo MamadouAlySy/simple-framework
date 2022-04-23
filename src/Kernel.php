821a6c42306f87eeeffeef24278950d438a3ffc7
Mamadou Aly Sy <sy.aly.mamadou@gmail.com>
2022-04-23 16:51:02 +0000

    feat: :recycle: refactor kernel

--- src/Kernel.php
+++ src/Kernel.php
@@ -7,24 +7,41 @@ namespace MamadouAlySy\SimpleFramework;
 use MamadouAlySy\SimpleFramework\Container\Container;
 use MamadouAlySy\SimpleFramework\Container\ContainerInterface;
 use MamadouAlySy\SimpleFramework\ControllerResolver\ControllerResolver;
+use MamadouAlySy\SimpleFramework\ControllerResolver\ControllerResolverInterface;
+use MamadouAlySy\SimpleFramework\DatabaseConnection\DatabaseConnection;
+use MamadouAlySy\SimpleFramework\DatabaseConnection\DatabaseConnectionInterface;
 use MamadouAlySy\SimpleFramework\Http\RequestInterface;
 use MamadouAlySy\SimpleFramework\Http\ResponseInterface;
+use MamadouAlySy\SimpleFramework\QueryBuilder\QueryBuilder;
+use MamadouAlySy\SimpleFramework\QueryBuilder\QueryBuilderInterface;
 use MamadouAlySy\SimpleFramework\Renderer\Renderer;
 use MamadouAlySy\SimpleFramework\Renderer\RendererInterface;
-use MamadouAlySy\SimpleFramework\Router\Router;
 use MamadouAlySy\SimpleFramework\Router\RouterInterface;
 
 class Kernel
 {
-    protected ContainerInterface $container;
-    protected Router $router;
+    protected static ContainerInterface $container;
 
     public function __construct(string $publicDir)
     {
         $this->createConstants($publicDir);
-        $this->container = new Container();
+        static::$container = new Container();
 
-        $this->container->makeSingleton(RendererInterface::class, new Renderer(VIEWS_DIR));
+        static::$container->makeSingleton(RendererInterface::class, new Renderer(VIEWS_DIR));
+        static::$container->makeSingleton(QueryBuilderInterface::class, new QueryBuilder());
+
+        static::$container->makeSingleton(DatabaseConnectionInterface::class, function () {
+            $databaseConfiguration = require_once CONFIG_DIR . DS . 'database.php';
+            return new DatabaseConnection($databaseConfiguration['mysql']);
+        });
+
+        static::$container->makeSingleton(RouterInterface::class, function () {
+            return require_once APP_DIR . DS . 'routes.php';
+        });
+
+        static::$container->makeSingleton(ControllerResolverInterface::class, function($container) {
+            return new ControllerResolver($container);
+        });
     }
 
     public function createConstants(string $publicDir): void
@@ -33,19 +50,16 @@ class Kernel
         define('ROOT_DIR', dirname($publicDir));
         define('APP_DIR', ROOT_DIR . DS . 'app');
         define('VIEWS_DIR', ROOT_DIR . DS . 'views');
+        define('CONFIG_DIR', ROOT_DIR . DS . 'config');
     }
 
     public function handle(RequestInterface $request): ResponseInterface
     {
-        // Get All routes
-        $this->router = require_once APP_DIR . DS . 'routes.php';
+        static::$container->makeSingleton(RequestInterface::class, $request);
 
-        $this->container->makeSingleton(RequestInterface::class, $request);
-        $this->container->makeSingleton(RouterInterface::class, $router);
-        
-        $controllerResolver = new ControllerResolver($this->container);
-        $route = $this->router->findRouteThatMatches($request);
-        // $middlewares = $route->getMiddlewares();
+        $controllerResolver = static::$container->get(ControllerResolverInterface::class);
+        $router = static::$container->get(RouterInterface::class);
+        $route = $router->findRouteThatMatches($request);
 
         return $controllerResolver->resolve($route->getCallable());
     }
@@ -55,8 +69,8 @@ class Kernel
      *
      * @return ContainerInterface
      */
-    public function getContainer(): ContainerInterface
+    public static function getContainer(): ContainerInterface
     {
-        return $this->container;
+        return static::$container;
     }
 }
